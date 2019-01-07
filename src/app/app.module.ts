5b2e1d6f95129549a876c354cbbee137a27586fe
Umer Abidin <umer.abidin@purelogics.net>
2019-01-07 15:01:11 +0500

    get user profile from facebook, store and get user information.

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Facebook } from '@ionic-native/facebook';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Facebook,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { ToastController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+profile_name: string;
+email_address: string;
+userData: any;
+profile_image: string;
+username: string;
+  constructor(public navCtrl: NavController, private facebook: Facebook,
+    private toastCtrl: ToastController,private http: HttpClient,private storage: Storage) {
+
+  }
+
+loginFb() {
+  this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+    this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+      this.userData = {email: profile['email'], first_name: profile['first_name'], last_name: profile['last_name'], 
+      picture: profile['picture_large']['data']['url'], username: profile['name']}
+this.email_address = this.userData.email;
+this.profile_name = this.userData.first_name; 
+this.profile_image = this.userData.picture;
+    });
+  });
+}
+
+storeData(){
+ this.storage.set('profile_name', this.profile_name);
+ this.storage.set('email_address', this.email_address);
+}
+
+getValues(){
+  this.storage.get('profile_name').then((val) => {
+    this.profile_name = val;
+
+  });
+
+  this.storage.get('email_address').then((val) => {
+    this.email_address = val;
+
+  });
+ }
+presentToast(message: string) {
+  let toast = this.toastCtrl.create({
+    message: message,
+    duration: 3000,
+    position: 'top'
+  });
+
+  toast.onDidDismiss(() => {
+    console.log('Dismissed toast');
+  });
+
+  toast.present();
+}
+}
